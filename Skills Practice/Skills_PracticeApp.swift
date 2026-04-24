6df262c01cd617db323365ea70aa744becd70450
nandoramz <i.framz13@gmail.com>
2026-04-24 08:52:03 -0700

    Initial Commit

---
+++ Skills Practice/Skills_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skills_PracticeApp.swift
+//  Skills Practice
+//
+//  Created by Fernando Ramirez on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct Skills_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
